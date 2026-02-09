fb37a8b77c1c9fc70089fa44f78d86b14fcd00f9
Adzari <azariraxa@gmail.com>
2026-02-09 11:38:23 +0200

    Create Footer

---
+++ frontend/src/components/Footer.tsx
@@ -0,0 +1,8 @@
+﻿
+const Footer = () => (
+    <footer className="bg-gray-800 text-white py-6 text-center">
+        &copy; {new Date().getFullYear()} ClubHub. All rights reserved. кчау
+    </footer>
+);
+
+export default Footer;
