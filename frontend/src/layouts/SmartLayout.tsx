48a688aaaf784313f400cb39fcb3489c85addfa9
Vasile Lazar <vlazar184@gmail.com>
2026-02-28 17:56:34 +0200

    Changed role-based routing to work as intended

---
+++ frontend/src/layouts/SmartLayout.tsx
@@ -0,0 +1,12 @@
+﻿import { useAuth } from '../hooks';
+import { GuestLayout } from './GuestLayout';
+import { UserLayout } from './UserLayout';
+import { AdminLayout } from './AdminLayout';
+
+export const SmartLayout: React.FC = () => {
+    const { user, isAuthenticated } = useAuth();
+
+    if (!isAuthenticated) return <GuestLayout />;
+    if (user?.role === 'admin') return <AdminLayout />;
+    return <UserLayout />;
+};
